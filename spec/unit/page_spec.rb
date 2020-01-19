a060e8ba250ce467cd7ff186ba08dec61293cefc
peter0083 <peter.tingyao@gmail.com>
2020-01-18 22:14:07 -0800

    update with course docker-compose.yml and page_spec.rb

--- spec/unit/page_spec.rb
+++ spec/unit/page_spec.rb
@@ -1,34 +1,27 @@
-# using Selenium to test 
-# Selenium is a web driver that spins up a real web browser like Chrome
-# and can get results from computed CSS
 require 'capybara'
 require 'capybara/dsl'
 require 'selenium-webdriver'
 
-include Capybara::DSL 
-Capybara.app_host = "http://website"
-Capybara.run_server = false
-# config capybara so that it runs selenium not rack_test
+include Capybara::DSL
+Capybara.app_host = "http://website" # Using Selenium; connect over network
+Capybara.run_server = false # Disable Rack since we are using Selenium.
 Capybara.register_driver :selenium do |app|
-    Capybara::Selenium::Driver.new(
-        app,
-        browser: :remote
-# tell selenium to NOT look for browser inside the container
-# get chrome from the selenium hub
-        url => "http://#{ENV['SELENIUN_HOST']}:#{ENV['SELENIUM_PORT']}/wd/hub",
-        desired_capabilities => Selenium::WebDriver::Remote::Capabilities.chrome(
-            "chromeOptions" => { "args" => [
-                    '--no-default-browser-check'
-                ]
-            }
-        )
+  Capybara::Selenium::Driver.new(
+    app,
+    browser: :remote,
+    url: "http://#{ENV['SELENIUM_HOST']}:#{ENV['SELENIUM_PORT']}/wd/hub",
+    desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
+      "chromeOptions" => {
+        "args" => ['--no-default-browser-check']
+      }
     )
+  )
 end
 Capybara.default_driver = :selenium
 
 describe "Example page render unit tests" do
-    it "should show the Explore California logo" do
-        visit('/')
-        expect(page.has_selector? 'a.logo').to be true 
-    end 
+  it "Shows the Explore California logo" do
+    visit('/')
+    expect(page.has_selector? '.logo').to be true
+  end
 end
